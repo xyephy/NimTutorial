8268fea5901d4a42968bc8a69b296c738043a536
Толли <ziphyr.xyeag@gmail.com>
2019-10-14 23:57:46 +0300

    Update immutableVariable.nim

--- variables/immutableVariable.nim
+++ variables/immutableVariable.nim
@@ -1,6 +1,7 @@
 #[
   # Immutable variables are variables whose values can't be changed
   # Declared using the 'let' keyword
-  # ]#
+ 
+ ]#
 let immutableVariable = "unchangeable value"
 echo immutableVariable
