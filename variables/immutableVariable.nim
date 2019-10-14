bd40579d068064b51dc7c058656900fe3aff57fc
xyephy <ziphyr.xyeag@gmail.com>
2019-10-14 23:55:59 +0300

    Variables: Immutable variables

---
+++ variables/immutableVariable.nim
@@ -0,0 +1,6 @@
+#[
+  # Immutable variables are variables whose values can't be changed
+  # Declared using the 'let' keyword
+  # ]#
+let immutableVariable = "unchangeable value"
+echo immutableVariable
