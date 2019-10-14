5956c7b154dec359b53f106e2a6a74bad8b13d61
xyephy <ziphyr.xyeag@gmail.com>
2019-10-14 23:47:21 +0300

    Variables: Mutable variables

---
+++ variables/mutableVariable.nim
@@ -0,0 +1,10 @@
+#[
+  #Mutable variables are variables whose value can be changed.
+  # They are declared using the 'Var' keyword.
+   ]#
+
+var mutableVariable = "Hello " # initialization
+echo mutableVariable
+
+
+
