8a5a2aa76c8f570c37fb7373f50575fdb0e8ff91
xyephy <ziphyr.xyeag@gmail.com>
2020-01-15 23:19:31 +0300

    ADD: DataTypes update

---
+++ DataTypes/dataTypes.nim
@@ -0,0 +1,16 @@
+var a: int
+var b = 10
+
+echo a
+
+var
+  d = 10
+  e = 5
+  w: int
+  q: float
+
+var someVariable: string = "some String"
+let i: float = 23.55
+
+echo someVariable.type
+echo i.type
