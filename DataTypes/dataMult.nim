0f5cbd0f1bc24edac38d525c3e01d43f6d362ad7
xyephy <ziphyr.xyeag@gmail.com>
2020-01-18 23:57:59 +0300

    Multiline data types

---
+++ DataTypes/dataMult.nim
@@ -0,0 +1,10 @@
+var a = """First line
+Second line
+Third line"""
+
+var someInteger: int = 10
+var someFloat: float = 5.25
+var someString: string = "A string"
+var someCharacter: char = 'a'
+var someBoolean: bool = true
+echo someInteger + someFloat.toInt
