662feab8485d9e7104b7c3ea33e1fbbd54cb1aeb
xyephy <ziphyr.xyeag@gmail.com>
2020-03-16 23:40:35 +0300

    conditional statements

---
+++ conditionalstatements/conditional.nim
@@ -0,0 +1,15 @@
+if 1 != 2:
+  echo "1 NOT EQUAL 2"
+if 1 != 3:
+  echo "1 NOT EQUAL 3"
+if 1 != 4:
+  echo "1 NOT EQUAL 4"
+
+if 1 != 2:
+  echo "1 NOT EQUAL 2"
+elif 1 != 3:
+  echo "1 NOT EQUAL 3"
+elif 1 != 4:
+  echo "1 NOT EQUAL 4"
+else:
+  echo "final statement"
